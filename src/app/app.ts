e7751c1249bec4b85ec5d6a64c608eee3e58995c
Walter <josuewalterespejoguarachi@gmail.com>
2026-06-09 20:43:36 -0400

    move tasks to app.ts fetch and change tasks in list-tasks attribute in input

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListTask } from './list-task/list-task';
 import { UpdateTaskDialog } from './update-task-dialog/update-task-dialog';
 import { CreateTaskDialog } from './create-task-dialog/create-task-dialog';
+import { Task } from '../repository/interface';
+import { TaskRepositoryHttp } from '../repository/http';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +13,27 @@ import { CreateTaskDialog } from './create-task-dialog/create-task-dialog';
   styleUrl: './app.css'
 })
 export class App {
+  // Dependences
+  private repository = inject(TaskRepositoryHttp)
+
+  // Signal
   protected readonly title = signal('task-app');
-  protected updatedTaskId: number | null = null;
+  protected tasks = signal<Task[]>([])
   protected openCreateTaskDialog = signal(false)
+
+  protected updatedTaskId: number | null = null;
+
+
+
+  ngOnInit() {
+    this.refresh()
+  }
+
+  refresh() {
+    console.log("Getting new tasks: ")
+    this.repository.getAllTasks().subscribe((tasks) => {
+      this.tasks.set(tasks)
+      console.log("Fetching tasks completed")
+    })
+  }
 }
